7d012d6eeceb2ce7af7b36ceb2644b139ea0c29a
datou666777 <1136615360@qq.com>
2018-02-22 20:37:37 +0800

    hello world

---
+++ test/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+
+        System.out.println("c=" + c);
+        System.out.println("Hello World!");
+    }
+}
